0915faafad7c87edaeb1e3cc3efb821a9f65e8cf
Daniel Fitze <daniel.fitze@psy.unibe.ch>
2022-10-05 18:10:25 +0200

    update

--- src/functions/read_folder.jl
+++ src/functions/read_folder.jl
@@ -1,29 +1,33 @@
-function read_folder(;path, header_row::Int = 1, limit = nothing, filename_to_col::Bool = true, split_filename::Bool = true, sep::String = "_")
+function read_folder(;path_in, path_out = nothing, header_row::Int = 1, limit = nothing, filename_to_col::Bool = true, split_filename::Bool = true, sep::String = "_")
   data = []
-  files = filter(endswith(".csv"), readdir(path))
+  files = filter(endswith(".csv"), readdir(path_in))
 
   for file in files
-    tmp = CSV.read(path*"/"*file, DataFrame, header = header_row, limit = limit)
-    
-    if split_filename
-      cols = replace(file, ".csv" => "")
-      cols = split(cols, sep)
-      colnumber = length(cols)
-      for col in cols
-        insertcols!(tmp, 1, Symbol("col_$(colnumber)") => col)
-        colnumber -= 1
-      end
-
-    end
+    tmp = CSV.read(path_in*"/"*file, DataFrame, header = header_row, limit = limit)
 
     if filename_to_col
-      insertcols!(tmp, 1, :filename => file)
+      if split_filename
+        cols = replace(file, ".csv" => "")
+        cols = split(cols, sep)
+        colnumber = length(cols)
+        for col in cols
+          insertcols!(tmp, 1, Symbol("col_$(colnumber)") => col)
+          colnumber -= 1
+        end
+      else
+        insertcols!(tmp, 1, :filename => file)
+      end
     end
 
     push!(data, tmp)
   end
 
   data = reduce(vcat, data)
+  
+  if !isnothing(path_out)
+    CSV.write(path_out, data)
+  end
+
   return data
 end
 
